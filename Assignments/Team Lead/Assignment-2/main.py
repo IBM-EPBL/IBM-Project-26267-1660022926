982b2e64673d150cba2c3d28ccd53656bd42b353
shreyamv <65845487+shreyamv@users.noreply.github.com>
2022-10-29 15:03:20 +0530

    Add files via upload

---
+++ Assignments/Team Lead/Assignment-2/main.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template
+import ibm_db
+try:
+    conn = ibm_db.pconnect("DATABASE=bludb;HOSTNAME=ba99a9e6-d59e-4883-8fc0-d6a8c9f7a08f.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=31321;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=rvw69738;PWD=pJbzIr1D8etDWbiZ",'','')
+    print(conn)
+except:
+    print(ibm_db.conn_errormsg())
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<p>Index Page!</p>"
+@app.route("/home")
+def HomePage():
+    return "<p>Home Page!</p>"
+@app.route("/about")
+def AboutPage():
+    return "<p>About Page!</p>"
+@app.route("/login")
+def LoginPage():
+    return render_template("login.html")
+@app.route("/signup")
+def SignupPage():
+    return render_template("signup.html")
